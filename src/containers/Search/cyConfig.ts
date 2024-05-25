ac4bcec9989faf5edf520e8f95b4d5a6d3820afc
Muhammad Hany <dev.mhany@gmail.com>
2024-05-26 02:07:06 +0300

    almost done

--- src/containers/Search/cyConfig.ts
+++ src/containers/Search/cyConfig.ts
@@ -1,49 +1,81 @@
-const cyConfig = (containerRef: any) => {
+import { Stylesheet, CytoscapeOptions, BaseLayoutOptions } from 'cytoscape'
+
+const cyConfig = (containerRef: any): CytoscapeOptions => {
+  const styles: Stylesheet[] = [
+    {
+      selector: 'node',
+      style: {
+        shape: 'round-rectangle',
+        'background-color': '#3E5348',
+        color: '#ecf0f1',
+        label: 'data(label)',
+        'font-weight': 'bold',
+        'text-halign': 'center',
+        'text-valign': 'center',
+        'text-wrap': 'wrap',
+        'font-size': '10px',
+        'border-width': '2px',
+        'border-color': '#3E5348',
+        'padding-left': '10px',
+        'padding-right': '10px',
+        'padding-top': '10px',
+        'padding-bottom': '10px'
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        width: 4,
+        'line-style': 'dashed',
+        'line-color': '#009688',
+        'target-arrow-shape': 'none',
+        'curve-style': 'taxi', // Change curve style to straight to simplify the edges
+        'line-dash-pattern': [10, 10],
+        'line-cap': 'round',
+        'font-size': '10px',
+        'text-rotation': 'autorotate',
+        'text-margin-x': 30,
+        'source-endpoint': 'outside-to-node',
+        'target-endpoint': 'outside-to-node'
+      }
+    }
+  ]
+
+  const layoutOptions: BaseLayoutOptions = {
+    name: 'breadthfirst',
+    fit: true, // whether to fit the viewport to the graph
+    directed: true, // whether the tree is directed downwards
+    padding: 30, // padding on fit
+    circle: false, // put depths in concentric circles if true, put depths top down if false
+    grid: true, // create an even grid into which the DAG is placed
+    spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+    roots: undefined, // the roots of the trees
+    depthSort: undefined, // a sorting function to order nodes at equal depth. e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+    animate: false, // whether to transition the node positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled
+
+    ready: undefined, // callback on layoutready
+    stop: undefined, // callback on layoutstop
+    transform: function (node, position) {
+      return position
+    } // transform a given node position
+  }
+
   return {
     container: containerRef || undefined,
     boxSelectionEnabled: false,
     autounselectify: true,
-    style: [
-      {
-        selector: 'node',
-        style: {
-          height: 300,
-          width: 300,
-          borderColor: '#00695f',
-          borderWidth: '10px',
-          borderOpacity: 0.5,
-          backgroundColor: '#009688',
-          color: '#fff',
-          fontWeight: 'bold',
-          fontSize: 25,
-          textHalign: 'center',
-          textValign: 'center',
-          textWrap: 'wrap',
-          textMaxWidth: 100,
-          content: 'data(label)'
-        }
-      },
-      {
-        selector: 'edge',
-        style: {
-          width: 6,
-          fontSize: 20,
-          targetArrowShape: 'triangle-cross',
-          lineColor: '#006400',
-          targetArrowColor: '#006400',
-          curveStyle: 'bezier',
-          controlPointWeights: '0.5 0.2 0.8',
-          textRotation: 'autorotate',
-          textMarginX: 30,
-          content: 'data(relation)'
-        }
-      }
-    ],
-    layout: {
-      name: 'breadthfirst',
-      directed: true,
-      padding: 10
-    }
+    style: styles,
+    layout: layoutOptions,
+    wheelSensitivity: 0.2,
+    minZoom: 0.1,
+    maxZoom: 2,
+    userZoomingEnabled: true,
+    userPanningEnabled: true
   }
 }
 
