baee38ba8016f1a188281790cf0363906c7df635
Muhammad Hany <dev.mhany@gmail.com>
2024-07-08 11:59:57 +0300

    New Graph design

--- src/containers/Search/index.tsx
+++ src/containers/Search/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useRef, useEffect } from 'react'
 import styles from './styles'
 import IHomeProps from './types'
 import Cytoscape from 'cytoscape'
@@ -5,15 +6,16 @@ import cyConfig from './cyConfig'
 import axios from '../../axiosInstance'
 import Modal from '../../components/Modal'
 import { useLocation } from 'react-router-dom'
-import { useState, useRef, useEffect } from 'react'
-import NewClaim from '../../components/NewClaim/AddNewClaim'
-import { parseSingleNode, parseMultipleNodes } from './graph.utils'
 import { useTheme, useMediaQuery, Container, Box } from '@mui/material'
+import GraphinfButton from './GraphInfButton'
+import NewClaim from './AddNewClaim'
+import { parseSingleNode, parseMultipleNodes } from './graph.utils'
+import 'cytoscape-node-html-label'
+import './CustomNodeStyles.css'
 
 const Search = (homeProps: IHomeProps) => {
   const search = useLocation().search
   const theme = useTheme()
-
   const { setLoading, setSnackbarMessage, toggleSnackbar } = homeProps
   const ref = useRef<any>(null)
   const query = new URLSearchParams(search).get('query')
@@ -22,16 +24,21 @@ const Search = (homeProps: IHomeProps) => {
   const [selectedClaim, setSelectedClaim] = useState<any>(null)
   const [cy, setCy] = useState<Cytoscape.Core>()
   const page = useRef(1)
-  const isArange = useMediaQuery('(min-width:700px) and (max-width:800px)')
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-  const special = useMediaQuery('(width:540px)')
+  const isMediumUp = useMediaQuery(theme.breakpoints.up('md'))
+
+  const layoutName = isMediumUp ? 'circle' : 'breadthfirst'
+  const layoutOptions = {
+    directed: !isMediumUp,
+    fit: true,
+    spacingFactor: isMediumUp ? 0.7 : 1.1
+  }
 
   const runCy = () => {
     if (!cy) return
     cy.layout({
-      name: 'circle',
-      // name: 'breadthfirst',
-      padding: isArange ? 110 : isSmallScreen ? (special ? 90 : 10) : 70,
+      name: layoutName,
+      ...layoutOptions,
+      padding: isMediumUp ? 80 : 0,
       animate: true,
       animationDuration: 1000
     }).run()
@@ -89,9 +96,6 @@ const Search = (homeProps: IHomeProps) => {
     const originalEvent = event.originalEvent
     event.preventDefault()
     if (originalEvent) {
-      // Your shift + click logic goes here...
-      // TODO refactor with handleMouseRightClick
-      // const claim = event.target
       const currentClaim = event.target.data('raw')
 
       if (currentClaim) {
@@ -162,11 +166,10 @@ const Search = (homeProps: IHomeProps) => {
 
   useEffect(() => {
     if (!cy) {
-      setCy(Cytoscape(cyConfig(ref.current)))
+      setCy(Cytoscape(cyConfig(ref.current, theme, layoutName, layoutOptions)))
     }
-  }, [])
+  }, [theme, isMediumUp])
 
-  //remove contextmenu
   useEffect(() => {
     document.addEventListener('contextmenu', event => event.preventDefault())
     return () => {
@@ -175,19 +178,22 @@ const Search = (homeProps: IHomeProps) => {
   }, [])
 
   return (
-    <Container sx={styles.container} maxWidth={false}>
-      <Modal open={openModal} setOpen={setOpenModal} selectedClaim={selectedClaim} />
-      <NewClaim
-        open={openNewClaim}
-        setOpen={setOpenNewClaim}
-        selectedClaim={selectedClaim}
-        setLoading={setLoading}
-        setSnackbarMessage={setSnackbarMessage}
-        toggleSnackbar={toggleSnackbar}
-      />
+    <>
+      <Container sx={styles.container} maxWidth={false}>
+        <Modal open={openModal} setOpen={setOpenModal} selectedClaim={selectedClaim} />
+        <NewClaim
+          open={openNewClaim}
+          setOpen={setOpenNewClaim}
+          selectedClaim={selectedClaim}
+          setLoading={setLoading}
+          setSnackbarMessage={setSnackbarMessage}
+          toggleSnackbar={toggleSnackbar}
+        />
 
-      <Box ref={ref} sx={styles.cy} />
-    </Container>
+        <Box ref={ref} sx={styles.cy} />
+      </Container>
+      <GraphinfButton />
+    </>
   )
 }
 
